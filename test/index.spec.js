cbfefd19d3082664b4df77a0cb548887b815019e
Anthony Urbina <aurbina.metro@gmail.com>
2017-07-03 10:54:58 -0400

    added mocha

---
+++ test/index.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import mocha, {describe, before, it} from 'mocha';
+
+describe("Canary test", () => {
+    it("should return true", () => {
+        expect(true).
+    })
+})
